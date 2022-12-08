104ff5919ba0be3df2a6f9cf4d071dffd8becf2f
CarolinaShlain123 <caroshlain@gmail.com>
2022-12-07 21:07:25 -0300

    retoques

--- preentrega1/src/componentes/ItemDetail/ItemDetail.js
+++ preentrega1/src/componentes/ItemDetail/ItemDetail.js
@@ -1,41 +1,39 @@
-import React, { useState} from 'react';
-import'./itemDetail.css'
+import React, { useState } from "react";
+import "./itemDetail.css";
 
-
-function ItemDetail ({prod}) {
-
+function ItemDetail({ prod }) {
   const [contador, setContador] = useState(0);
 
-  const botonAumentar =()=> {
-    setContador(contador +1)
-    if(contador>= prod.stock){
-      botonBajar()
-    }else{
-      botonAumentar()
+  const botonAumentar = () => {
+    setContador(contador + 1);
+    if (contador >= prod.stock) {
+      botonBajar();
+    } else {
+      botonAumentar();
     }
-  } 
-  const botonBajar =()=> {
-    setContador(contador -1)
-    if(contador<=0){
-botonAumentar()
-    }else{
-      botonBajar()
+  };
+  const botonBajar = () => {
+    setContador(contador - 1);
+    if (contador <= 0) {
+      botonAumentar();
+    } else {
+      botonBajar();
     }
-  } 
-  return(
-    <div className='contenedorDetalle'>
-      <div className='items'>
-      <h2 className='title'>{prod.nombre} </h2>
-      <img className='imagen' src={prod.image} alt={prod.id} />
-      <p className='precio'>${prod.precio} </p>
+  };
+  return (
+    <div className="contenedorDetalle">
+      <div className="items">
+        <h2 className="title">{prod.nombre} </h2>
+        <img className="imagen" src={prod.image} alt={prod.id} />
+        <p className="precio">${prod.precio} </p>
       </div>
-      <div className='botoncant'>
-      <button onClick= {botonAumentar}>+</button>
-      <div className='cantidad'>Cantidad:{contador}</div>
-      <button onClick= {botonBajar}>-</button>
+      <div className="botoncant">
+        <button onClick={botonAumentar}>+</button>
+        <div className="cantidad">Cantidad:{contador}</div>
+        <button onClick={botonBajar}>-</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default ItemDetail;
